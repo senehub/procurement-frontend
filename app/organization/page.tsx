c10c9250124cb7bba11bc1b05cec9dc764cd85a7
Touray Mahammed <toure925@outlook.com>
2024-12-08 04:13:11 +0000

    updated pnpm lock file

--- app/organization/page.tsx
+++ app/organization/page.tsx
@@ -16,7 +16,7 @@ export default function OrganizationPage() {
     <PageLayout>
       <PageHeader heading="Organization" />
       <PageContent>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           <Card>
             <CardHeader>
               <CardTitle>Staffs</CardTitle>
